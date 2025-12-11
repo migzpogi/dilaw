f039c40d8d35f98a4e83daad4b4e7ba87494d70d
migzpogi <migz.zgim@gmail.com>
2025-12-11 14:11:41 +0800

    Adds WL None handling. Uses previous game's result if current game is not yet updated in the API

--- api/lakers.py
+++ api/lakers.py
@@ -3,30 +3,59 @@ from nba_api.stats.static import teams
 from nba_api.stats.endpoints import leaguegamefinder
 
 
-def did_lakers_win():
+def did_lakers_win(last_x_games=5):
     lal_id = teams.find_team_by_abbreviation('LAL')['id']
     game_finder = leaguegamefinder.LeagueGameFinder(team_id_nullable=lal_id)
     all_games = game_finder.get_data_frames()[0]
-    most_recent = all_games[all_games.SEASON_ID.str[-4:] == '2025'][:1]
+    most_recent = all_games[all_games.SEASON_ID.str[-4:] == '2025'][:last_x_games]
 
-    box = boxscore.BoxScore(game_id=most_recent['GAME_ID'][0]).get_dict()
-    print(box['game']['homeTeam']['teamTricode'])
-    print(box['game']['homeTeam']['score'])
+    list_of_previous_games = []
 
-    summary = {
-        'won': None,
-        'matchup': most_recent['MATCHUP'][0],
-        'game_date': most_recent['GAME_DATE'][0],
-        'home_score': box['game']['homeTeam']['score'],
-        'away_score': box['game']['awayTeam']['score'],
-        'home_team': box['game']['homeTeam']['teamTricode'],
-        'away_team': box['game']['awayTeam']['teamTricode']
-    }
+    for idx, game_id in enumerate(most_recent['GAME_ID']):
+        box = boxscore.BoxScore(game_id=game_id).get_dict()
 
-    if most_recent['WL'][0].upper() == 'W':
-        summary['won'] = True
-    else:
-        summary['won'] = False
+        summary = {
+            'won': None,
+            'matchup': most_recent['MATCHUP'][idx],
+            'game_date': most_recent['GAME_DATE'][idx],
+            'home_score': box['game']['homeTeam']['score'],
+            'away_score': box['game']['awayTeam']['score'],
+            'home_team': box['game']['homeTeam']['teamTricode'],
+            'away_team': box['game']['awayTeam']['teamTricode']
+        }
 
-    return summary
+        if most_recent['WL'][idx] is not None:
+            if most_recent['WL'][idx].upper() == 'W':
+                summary['won'] = True
+            else:
+                summary['won'] = False
+        else:
+            summary['won'] = None
 
+        list_of_previous_games.append(summary)
+
+    for result in list_of_previous_games:
+        if result['won'] is not None:
+            return result
+
+    # box = boxscore.BoxScore(game_id=most_recent['GAME_ID'][0]).get_dict()
+    # print(box['game']['homeTeam']['teamTricode'])
+    # print(box['game']['homeTeam']['score'])
+
+    # summary = {
+    #     'won': None,
+    #     'matchup': most_recent['MATCHUP'][0],
+    #     'game_date': most_recent['GAME_DATE'][0],
+    #     'home_score': box['game']['homeTeam']['score'],
+    #     'away_score': box['game']['awayTeam']['score'],
+    #     'home_team': box['game']['homeTeam']['teamTricode'],
+    #     'away_team': box['game']['awayTeam']['teamTricode']
+    # }
+
+    #
+    # if most_recent['WL'][0].upper() == 'W':
+    #     summary['won'] = True
+    # else:
+    #     summary['won'] = False
+    #
+    # return summary
